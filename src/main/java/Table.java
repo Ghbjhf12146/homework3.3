208fa60e3b97db926b7738ea9e589922a01dcca8
oleg <adshepelev@gmail.com>
2023-11-06 23:34:32 +0300

    1

--- src/main/java/Table.java
+++ src/main/java/Table.java
@@ -2,7 +2,7 @@ public class Table {
     public static void main(String[] args) {
         for (int i = 1; i < 10; i++) {
             System.out.println("\n");
-            for (int j = i; 10 > j; j++) {
+            for (int j = i; j< 10; j++) {
                 if ((i * j) < 9)
                     System.out.println("" + "" + "" + (i * j));
                 else
